8e9f206c8605c775122158ca6f67efd31aab5a5c
Lennart Mol <lc.mol@student.avans.nl>
2024-03-07 12:07:29 +0100

    Examples for face and person detection working

---
+++ examples/yolo_detection_demo/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Espressif/frameworks/esp-idf-v5.1/components/bootloader/subproject"
+  "C:/Users/lenna/Documents/GitHub/ESP32S3-People-Detection-Demo/examples/yolo_detection_demo/build/bootloader"
+  "C:/Users/lenna/Documents/GitHub/ESP32S3-People-Detection-Demo/examples/yolo_detection_demo/build/bootloader-prefix"
+  "C:/Users/lenna/Documents/GitHub/ESP32S3-People-Detection-Demo/examples/yolo_detection_demo/build/bootloader-prefix/tmp"
+  "C:/Users/lenna/Documents/GitHub/ESP32S3-People-Detection-Demo/examples/yolo_detection_demo/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/Users/lenna/Documents/GitHub/ESP32S3-People-Detection-Demo/examples/yolo_detection_demo/build/bootloader-prefix/src"
+  "C:/Users/lenna/Documents/GitHub/ESP32S3-People-Detection-Demo/examples/yolo_detection_demo/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Users/lenna/Documents/GitHub/ESP32S3-People-Detection-Demo/examples/yolo_detection_demo/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Users/lenna/Documents/GitHub/ESP32S3-People-Detection-Demo/examples/yolo_detection_demo/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
